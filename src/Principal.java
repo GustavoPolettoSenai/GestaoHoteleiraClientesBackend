b300c2dfbf831c99ac441db372343c9c70054b91
GustavoPolettoSenai <gustavo_poletto@estudante.sesisenai.org.br>
2024-11-12 20:23:19 -0300

    Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -8,23 +8,30 @@ import test.ClienteTest;
 public class Principal {
 
     public static void main(String[] args) throws PessoaException {
-        // Inicialização de objetos
+        // Cria um objeto para acessar o banco de dados
         ClienteDAO clienteDAO = new ClienteDAO();
+
+        // Cria um objeto de serviço que usa o acesso ao banco de dados
         ClienteService clienteService = new ClienteService(clienteDAO);
+
+        // Cria um objeto para testar as funcionalidades do serviço
         ClienteTest clienteTest = new ClienteTest(clienteService);
+
+        // Cria o controlador que vai gerenciar os testes
         ClienteController clienteController = new ClienteController(clienteTest);
 
-        // Informações do teste
-        Funcionalidade funcionalidade = Funcionalidade.ALTERAR;
+        // Escolhe a funcionalidade que será testada
+        Funcionalidade funcionalidade = Funcionalidade.LISTAR;
         System.out.println("CLIENTE: " + funcionalidade);
         System.out.println("RESULTADO DO TESTE:");
 
-        // Realização do teste
+        // Executa o teste para a funcionalidade escolhida
         try {
+            // Mostra o resultado do teste
             System.out.println(clienteController.testar(funcionalidade));
         } catch (PessoaException excecao) {
+            // Se houver um erro, mostra a mensagem de erro
             System.err.println(excecao.getMessage());
         }
     }
 }
-
