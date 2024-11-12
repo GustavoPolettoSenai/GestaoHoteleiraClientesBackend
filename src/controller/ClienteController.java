78da90f10a3bb20841cf7593102fc2fe1112c6b6
GustavoPolettoSenai <gustavo_poletto@estudante.sesisenai.org.br>
2024-11-12 20:23:48 -0300

    ClienteController.java

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -6,26 +6,32 @@ import test.ClienteTest;
 
 public class ClienteController {
 
-    // Atributos
+    // Atributo que guarda o objeto de teste
     private ClienteTest clienteTest;
 
-    // Construtor
+    // Construtor que recebe um objeto de teste
     public ClienteController(ClienteTest clienteTest) {
         this.clienteTest = clienteTest;
     }
 
-    // Gerenciador de testes
+    // Método que gerencia qual teste será executado
     public String testar(Funcionalidade funcionalidade) throws PessoaException {
+        // Verifica qual funcionalidade foi escolhida
         switch (funcionalidade) {
             case LISTAR:
+                // Executa o teste de listar clientes
                 return clienteTest.listar();
             case CADASTRAR:
+                // Executa o teste de cadastrar cliente
                 return clienteTest.cadastrar();
             case ALTERAR:
+                // Executa o teste de alterar cliente
                 return clienteTest.alterar();
             case EXCLUIR:
+                // Executa o teste de excluir cliente
                 return clienteTest.excluir();
             default:
+                // Retorna nulo se a funcionalidade não for reconhecida
                 return null;
         }
     }
